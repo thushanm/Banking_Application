7ad8171b91e7c0d3487e25a8694dfa729eeea068
thushanm <thushanmadhushara2@gmail.com>
2025-02-28 23:31:56 +0530

    add more services

---
+++ backend/api-Gateway/src/test/java/com/thushan/api/Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thushan.api.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
